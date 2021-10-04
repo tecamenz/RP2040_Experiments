5ae9f0fa3e6c80fa1f04ac021061a117a8e9a246
tecamenz <martin.camenzind@protonmail.ch>
2021-10-04 22:21:49 +0200

    second test

---
+++ sdtest2.py
@@ -0,0 +1,39 @@
+"""
+Example code for writing and reading from an SD-Card
+
+"""
+import machine
+import sdcard
+import uos
+
+# Assign chip select (CS) pin (and start it high)
+cs = machine.Pin(9, machine.Pin.OUT)
+
+# Intialize SPI peripheral (start with 1 MHz)
+spi = machine.SPI(1,
+                  baudrate=1000000,
+                  polarity=0,
+                  phase=0,
+                  bits=8,
+                  firstbit=machine.SPI.MSB,
+                  sck=machine.Pin(14),
+                  mosi=machine.Pin(15),
+                  miso=machine.Pin(12))
+
+# Initialize SD card
+sd = sdcard.SDCard(spi, cs)
+
+# Mount filesystem
+vfs = uos.VfsFat(sd)
+uos.mount(vfs, "/sd")
+
+# Create a file and write something to it
+with open("/sd/test01.txt", "a") as file:
+    file.write("Hello, SD World!\r\n")
+    file.write("This is a test\r\n")
+
+# Open the file we just created and read from it
+with open("/sd/test01.txt", "r") as file:
+    data = file.read()
+    print(data)
+print('End')
